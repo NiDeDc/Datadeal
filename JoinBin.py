b8f4c4d349a00e5d487cdfbc52ad2f3e18e54267
NiDeDc <zaq515444636@qq.com>
2022-05-31 11:30:05 +0800

    按仪表通道独立合并bin文件

--- JoinBin.py
+++ JoinBin.py
@@ -5,7 +5,8 @@ newFileName = ''
 newFile = None
 files = os.listdir(filePath)
 for file in files:
-    if os.path.isfile(os.path.join(filePath, file)) and file.endswith('.bin'):
+    if os.path.isfile(os.path.join(filePath, file)) and file.endswith('.bin') \
+            and file[0] == 'D' and file[2:4] == "_C":
         if newFileName == '':
             newFileName = file
             newFilePath = os.path.join(filePath, 'new')
@@ -17,9 +18,23 @@ for file in files:
                 print('remove file', os.path.join(newFilePath, newFileName))
             newFile = open(os.path.join(newFilePath, newFileName), 'ab+')
             print("open newFile", newFilePath, newFileName)
+        else:
+            if file[0:5] != newFileName[0:5]:
+                newFile.close()
+                newFileName = file
+                newFilePath = os.path.join(filePath, 'new')
+                if not os.path.exists(newFilePath):
+                    os.makedirs(newFilePath)
+                    print('makedir', newFilePath)
+                if os.path.exists(os.path.join(newFilePath, newFileName)):
+                    os.remove(os.path.join(newFilePath, newFileName))
+                    print('remove file', os.path.join(newFilePath, newFileName))
+                newFile = open(os.path.join(newFilePath, newFileName), 'ab+')
+                print("open newFile", newFilePath, newFileName)
         curFile = open(os.path.join(filePath, file), 'rb')
         print('open curFile' ,file)
         data = curFile.read()
         newFile.write(data)
         curFile.close()
-newFile.close()
+if newFile is not None:
+    newFile.close()
