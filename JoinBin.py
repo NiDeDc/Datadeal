c99cb7448e5c5a26a51b7dae438f97eb72fdb1a2
NiDeDc <zaq515444636@qq.com>
2022-06-06 11:43:15 +0800

    按下划线拆分获取信号

--- JoinBin.py
+++ JoinBin.py
@@ -6,7 +6,7 @@ newFile = None
 files = os.listdir(filePath)
 for file in files:
     if os.path.isfile(os.path.join(filePath, file)) and file.endswith('.bin') \
-            and file[0] == 'D' and file[2:4] == "_C":
+            and file.split('_')[0][0] == 'D' and file.split('_')[1][0] == 'C':
         if newFileName == '':
             newFileName = file
             newFilePath = os.path.join(filePath, 'new')
@@ -19,7 +19,8 @@ for file in files:
             newFile = open(os.path.join(newFilePath, newFileName), 'ab+')
             print("open newFile", newFilePath, newFileName)
         else:
-            if file[0:5] != newFileName[0:5]:
+            if file.split('_')[0] != newFileName.split('_')[0] \
+                    or file.split('_')[1] != newFileName.split('_')[1]:
                 newFile.close()
                 newFileName = file
                 newFilePath = os.path.join(filePath, 'new')
